ad62c698fbad3ac294c1d3ffc35efc9fb4560ee4
cwkang1 <cheolwoo1714@gmail.com>
2026-09-04 07:47:36 +0900

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAND_VOTE_CONFIG = {
-  supabaseUrl: 'https://evtognvxqxhbuspfbvqg.supabase.co',
+  supabaseUrl: 'https://evtognvxgxhbuspfbvqg.supabase.co',
   supabaseAnonKey: 'sb_publishable_oe9ernwONvRfFwsZnp8xDg_PidpgSJ_'
 };
